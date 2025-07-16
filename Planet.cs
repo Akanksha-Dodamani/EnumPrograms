2bdc03a2a56492d0584915da2489f1585853ab93
Akanksha-Dodamani <akanksha.dodamani@aurionpro.com>
2025-07-16 10:50:18 +0530

    Implement Planet enum

---
+++ Planet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPrograms
+{
+    public class Planet
+    {
+        public enum Planets { Earth, Saturn, Neptune }
+        public static double GetGravity(Planets planet)
+        {
+            if (planet == Planets.Saturn)
+            {
+                return 1.08;
+            }
+            else if (planet == Planets.Neptune)
+            {
+                return 1.14;
+            }
+            else
+            {
+                return 1.0;
+            }
+        }
+        public static void WeightOnPlanets()
+        {
+            Console.Write("Enter weight on Earth: ");
+            double weight = double.Parse(Console.ReadLine());
+
+            double saturnWeight = weight * GetGravity(Planets.Saturn);
+            double neptuneWeight = weight * GetGravity(Planets.Neptune);
+
+            Console.WriteLine("Weight on Saturn: " + saturnWeight + " kg");
+            Console.WriteLine("Weight on Neptune: " + neptuneWeight + " kg");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ namespace EnumPrograms
             {
                 Console.WriteLine("Invalid signal input.");
             }
+
+            // PLANET ENUM
+            Planet.WeightOnPlanets();
         }
     }
 }
